7d139a9e53a44c7a72fbc75d17a43738e5969fe2
Jiwoong-Jung <jjw981119@naver.com>
2023-09-26 11:43:39 +0900

    Member, Team 추가

--- src/main/java/project1/mongo/controller/MyController.java
+++ src/main/java/project1/mongo/controller/MyController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import project1.mongo.repository.MyDataMongo;
 import project1.mongo.repository.MyDataMongoRepository;
 
@@ -19,6 +20,10 @@ public class MyController {
 
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("title", "Find Page");
+        model.addAttribute("msg", "MyDataMongo");
+        List<MyDataMongo> list = myDataMongoRepository.findAll();
+        model.addAttribute("datalist", list);
         return "index";
     }
     @PostMapping("/")
@@ -29,12 +34,32 @@ public class MyController {
         return "redirect:/";
     }
 
-    @PostMapping("/find")
+    @GetMapping("/find")
     public String find(Model model) {
         model.addAttribute("title", "Find Page");
         model.addAttribute("msg", "MyData 예제");
         List<MyDataMongo> list = myDataMongoRepository.findAll();
         model.addAttribute("datalist", list);
         return "find";
+    }
+    @PostMapping("/find")
+    public String search(@RequestParam("find") String param, Model model) {
+        if ("".equals(param)) {
+            return "redirect:/find";
+        } else {
+            model.addAttribute("title", "Find result");
+            model.addAttribute("msg", param + "의 검색 결과");
+            model.addAttribute("value", param);
+//            List<MyDataMongo> list = myDataMongoRepository.findByNameLike(param);
+            List<MyDataMongo> list = myDataMongoRepository.listByName(param);
+            model.addAttribute("datalist", list);
+            return  "find";
+        }
+    }
+
+    @GetMapping("/view")
+    @ResponseBody
+    public List<MyDataMongo> view() {
+        return myDataMongoRepository.findAll();
     }
 }
