c50ddaeb17348987dd5d8745a941e85c52e851b6
Jiwoong-Jung <jjw981119@naver.com>
2023-09-26 10:54:04 +0900

    리포지토리 생성

--- src/main/java/project1/mongo/controller/MyController.java
+++ src/main/java/project1/mongo/controller/MyController.java
@@ -1,14 +1,40 @@
 package project1.mongo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import project1.mongo.repository.MyDataMongo;
+import project1.mongo.repository.MyDataMongoRepository;
+
+import java.util.List;
 
 @Controller
 public class MyController {
 
+    @Autowired
+    MyDataMongoRepository myDataMongoRepository;
+
     @GetMapping("/")
     public String index(Model model) {
         return "index";
+    }
+    @PostMapping("/")
+    public String form(@RequestParam String name,
+                       @RequestParam String memo) {
+        MyDataMongo myDataMongo = new MyDataMongo(name, memo);
+        myDataMongoRepository.save(myDataMongo);
+        return "redirect:/";
+    }
+
+    @PostMapping("/find")
+    public String find(Model model) {
+        model.addAttribute("title", "Find Page");
+        model.addAttribute("msg", "MyData 예제");
+        List<MyDataMongo> list = myDataMongoRepository.findAll();
+        model.addAttribute("datalist", list);
+        return "find";
     }
 }
